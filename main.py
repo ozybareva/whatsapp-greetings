99b31f1282ddd207f0ad3908acb67874261cfde7
oytarhanova <oytarhanova@fintechiq.ru>
2024-02-02 09:36:14 +0700

    move class initialization from main

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from whatsapp_api_client_python.API import GreenApi
-import settings
+from clients.whatsapp_client import WhatsAppClient
+from settings import settings
 
-
-class WhatsAppClient:
-    def __init__(self):
-        self.settings = settings.Settings()
-        self.restApi = GreenApi(
-            host=self.settings.wa_host,
-            media=self.settings.wa_media_host,
-            idInstance=self.settings.wa_id_instance,
-            apiTokenInstance=self.settings.wa_api_token_instance
-        )
-
-
-wa_client = WhatsAppClient()
+wa_client = WhatsAppClient(settings)
 result = wa_client.restApi.serviceMethods.getContacts().data
+
 
